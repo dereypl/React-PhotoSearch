ddf668653b3cea6ccab99f284a798f307849ca91
dereypl <mpasich.pk@gmail.com>
2019-08-11 18:25:49 +0200

    fix grid bug and add semantic input look

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -13,11 +13,14 @@ class SearchBar extends React.Component {
 
     render () {
         return <div className="ui segment">
-            <form onSubmit={this.onFormSubmit} className="ui from">
+            <form className="ui from" onSubmit={this.onFormSubmit}>
                 <div className="field">
-                    <label>Image Search</label>
-                <input type="text" value={this.state.term} onChange={e => this.setState({term: e.target.value})}/>
+                    <label>Image Search: </label>
+                    <div className="ui input">
+                <input placeholder="Search your photo..." type="text" value={this.state.term} onChange={e => this.setState({term: e.target.value})}/>
                 </div>
+                </div>
+
             </form>
         </div>
     }

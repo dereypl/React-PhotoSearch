79ab151499c0539f9254d3b98d6c0085505d3229
dereypl <mpasich.pk@gmail.com>
2019-08-11 18:41:22 +0200

    Updates

---
+++ precache-manifest.ad6b44bd79ee9a99e1c5d828e44ff26a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8874b4a6b1b0283533fc445ff29688f1",
+    "url": "/React-PhotoSearch/index.html"
+  },
+  {
+    "revision": "ad8e9e2bca99db997c4e",
+    "url": "/React-PhotoSearch/static/css/main.8a8b11b9.chunk.css"
+  },
+  {
+    "revision": "50ab50fbb9626220e093",
+    "url": "/React-PhotoSearch/static/js/2.7df7d174.chunk.js"
+  },
+  {
+    "revision": "ad8e9e2bca99db997c4e",
+    "url": "/React-PhotoSearch/static/js/main.c1618088.chunk.js"
+  },
+  {
+    "revision": "340a234f7ceccd342bb0",
+    "url": "/React-PhotoSearch/static/js/runtime~main.8f8f7607.js"
+  }
+]);
